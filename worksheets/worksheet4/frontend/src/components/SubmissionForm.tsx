7ce6a68ba54d22e854c60a3f6c77e12e80a7a62a
yhkim8046 <yhkim8046@gmail.com>
2024-08-11 20:16:08 +1200

    Conflicts in settings removed

--- worksheets/worksheet4/frontend/src/components/SubmissionForm.tsx
+++ worksheets/worksheet4/frontend/src/components/SubmissionForm.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { useForm } from "react-hook-form";
+import { useForm } from 'react-hook-form';
 
 export default function SubmissionForm() {
   const { register, handleSubmit } = useForm();
