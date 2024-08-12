b4a75b0a4e36ac24ce3e843731e1fa16031d2d41
yhkim8046 <yhkim8046@gmail.com>
2024-08-12 23:11:26 +1200

    CRUD made
    CRUD made

--- worksheets/worksheet4/frontend/src/pages/_app.tsx
+++ worksheets/worksheet4/frontend/src/pages/_app.tsx
@@ -1,6 +1,15 @@
-import "@/styles/globals.css";
+import "../styles/globals.scss";
 import type { AppProps } from "next/app";
+import { SessionProvider } from "next-auth/react";
+import PopulatedNavBar from "@/components/PopulatedNavBar";
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+function MyApp({ Component, pageProps: { session, ...pageProps } }: AppProps) {
+  return (
+    <SessionProvider session={session}>
+      <PopulatedNavBar />
+      <Component {...pageProps} />
+    </SessionProvider>
+  );
 }
+
+export default MyApp;
